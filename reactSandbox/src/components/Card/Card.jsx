24dde6351d837a59be0a8762575ba3f60cee311f
Bruno Peres <bruno_16515@hotmail.com>
2022-05-30 19:01:27 -0300

    Construindo Modal

--- reactSandbox/src/components/Card/Card.jsx
+++ reactSandbox/src/components/Card/Card.jsx
@@ -1,12 +1,12 @@
 import './card.css'
 
-export function Card(props){
+export function Card({id, task, time, deleteFunc}){
 
   return (
     <div className="card">
-      <p>{props.id} - {props.task}</p>
-      <small>Criado em {props.time}</small>
-      <button onClick={() => props.deleteFunc(props.id)}><i className="fa-solid fa-trash-can"></i></button>
+      <p>{id} - {task}</p>
+      <small>Criado em {time}</small>
+      <button onClick={() => deleteFunc(id)}><i className="fa-solid fa-trash-can"></i></button>
     </div>
   )
 }
--- reactSandbox/src/pages/Home/Home.jsx
+++ reactSandbox/src/pages/Home/Home.jsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import { Card } from "../../components/Card/Card";
+import { Modal } from "../../components/Modal/Modal";
 import "./home.css";
 
 export function Home() {
+  const [showModal, setShowModal] = useState();
   const [showToDos, setShowToDos] = useState(true);
   const [toDo, setToDo] = useState();
   const [toDoList, setToDoList] = useState([]);
   const form = document.querySelector(".addTodo");
+
+  const handleCloseModal = () => setShowModal(false);
 
   const handleDelete = id =>
     setToDoList(prevState => prevState.filter(toDo => toDo.id != id));
 
   const handleAddTodo = e => {
     e.preventDefault();
-    if(form.addInput.value.length > 3) {
+    if (form.addInput.value.length > 3) {
       form.reset();
-  
+
       const newToDo = {
         task: toDo,
         time: new Date().toLocaleTimeString("pt-BR", {
@@ -26,6 +30,8 @@ export function Home() {
         id: toDoList.length + 1,
       };
       setToDoList(prevState => [...prevState, newToDo]);
+    } else {
+      setShowModal(true);
     }
   };
 
@@ -56,13 +62,22 @@ export function Home() {
           ))}
         {showToDos && (
           <form className="addTodo">
-            <input type="text" onChange={e => setToDo(e.target.value)} id="addInput" />
+            <input
+              type="text"
+              onChange={e => setToDo(e.target.value)}
+              id="addInput"
+            />
             <button type="submit" onClick={e => handleAddTodo(e)}>
               Adicionar
             </button>
           </form>
         )}
       </div>
+      {showModal && (
+        <Modal closeModal={handleCloseModal}>
+          <h2>Sua tarefa deve conter pelo menos 3 letras =&#41;</h2>
+        </Modal>
+      )}
     </section>
   );
 }
