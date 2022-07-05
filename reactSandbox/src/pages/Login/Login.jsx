4b017262bc2c9a9d7a357ce6e67c58cfd1837133
Bruno Peres <bruno_16515@hotmail.com>
2022-07-05 17:04:32 -0300

    Criando form login e signup

--- reactSandbox/src/pages/Login/Login.jsx
+++ reactSandbox/src/pages/Login/Login.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./Login.css";
 
 export function Login() {
   return (
-    <div>
-      LOGIN
-    </div>
-  )
+    <form className="signup-form">
+      <h2 className="form-title">Entrar</h2>
+      <label>
+        <p>email:</p>
+        <input type="email" onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        <p>senha:</p>
+        <input type="password" onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button>Entrar</button>
+    </form>
+  );
 }
--- reactSandbox/src/pages/Signup/Signup.jsx
+++ reactSandbox/src/pages/Signup/Signup.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./Signup.css";
 
 export function Signup() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
-        error officia saepe optio tempora, praesentium laboriosam iusto ducimus,
-        illum debitis excepturi accusantium nobis voluptas eveniet modi minima
-        nesciunt provident.
-      </p>
-    </div>
+    <form className="signup-form">
+      <h2 className="form-title">Cadastre-se:</h2>
+      <label>
+        <p>nome:</p>
+        <input type="text" onChange={e => setName(e.target.value)} />
+      </label>
+      <label>
+        <p>email:</p>
+        <input type="email" onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        <p>senha:</p>
+        <input type="password" onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button>Cadastrar</button>
+    </form>
   );
 }
